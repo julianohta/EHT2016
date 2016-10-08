79304dc90e529c60a2b6a687621eba86f696f92b
Harrison Unruh <harrison.unruh@gmail.com>
2016-10-08 12:20:30 -0400

    Setup templates, started building a report form

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.update(
+    DEBUG=True,
+)
 
 @app.route('/')
 def index():
     return 'ok'
+
+@app.route('/report')
+def report():
+    # db = get_db()
+    # cur = db.execute('select title, text from entries order by id desc')
+    # entries = cur.fetchall()
+    return render_template('report.html')
 
 if __name__ == "__main__":
     app.run()
