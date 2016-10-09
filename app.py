30f33092014c2d37adbc26a8bc2e40a31e557dcd
Julian Ohta <julianohta@gmail.com>
2016-10-09 08:01:35 -0400

    implemented messaging

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # all the imports
-from sms import sms_app
-import os
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
+from flask import Flask, session, g, redirect, url_for, abort, \
      render_template, flash
+import os
+from flask import jsonify, request
+from faker import Factory
+from twilio.access_token import AccessToken, IpMessagingGrant
+from flask_mail import Message, Mail
+from twilio.rest import TwilioRestClient
+import sqlite3
 
 
 app = Flask(__name__)
-app.register_blueprint(sms_app)
 
 
 # create our little application :)
@@ -16,16 +19,31 @@ app.config.update(
     DEBUG=True,
 )
 
+
+
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=os.path.join(app.root_path, 'app.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='default',
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USE_SSL=False,
+    MAIL_USERNAME='demomododo@gmail.com',
+    MAIL_PASSWORD='hackathons',
 ))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_envvar('APP_SETTINGS', silent=True)
 
 
+mail = Mail(app)
+
+
+fake = Factory.create()
+
+
+#db functions
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
@@ -64,6 +82,70 @@ def initdb_command():
     print 'Initialized the database.'
 
 
+# twilio stuff
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/text')
+def send_text():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    else:
+        sid = session.get('sid')
+        tkn = session.get('token')
+        print(sid)
+        print(tkn)
+        print("++++++++++++++++++++++++++++++")
+        client = TwilioRestClient(account=sid, token=tkn)
+        message = client.messages.create(to="+16073398907", from_="+16073912565", body="from a monkey with a typewriter")
+        return redirect(url_for('login'))
+
+@app.route('/token')
+def token():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    else:
+
+        # get credentials for environment variables
+        account_sid = session.get('sid')
+        print(session.get('sid'))
+        api_key = os.environ['TWILIO_API_KEY']
+        api_secret = os.environ['TWILIO_API_SECRET']
+        service_sid = os.environ['TWILIO_IPM_SERVICE_SID']
+
+        # create a randomly generated username for the client
+        db = get_db()
+        username = session.get('user')
+
+        identity = username
+
+        # Create a unique endpoint ID for the
+        device_id = request.args.get('device')
+        endpoint = "TwilioChatDemo:{0}:{1}".format(identity, device_id)
+
+        # Create access token with credentials
+        token = AccessToken(account_sid, api_key, api_secret, identity)
+
+        # Create an IP Messaging grant and add to token
+        ipm_grant = IpMessagingGrant(endpoint_id=endpoint, service_sid=service_sid)
+        token.add_grant(ipm_grant)
+
+        # Return token info as JSON
+        return jsonify(identity=identity, token=token.to_jwt())
+
+
+# messaging
+@app.route('/mail')
+def send_mail():
+    msg = Message('yo', sender="demomododo@gmail.com")
+    msg.add_recipient("demomododo@gmail.com")
+    mail.send(msg)
+
+    return redirect(url_for('login'))
+
+
 # display table
 @app.route('/view')
 def show_entries():
@@ -87,6 +169,7 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 
+#please don't hack :]
 @app.route('/register', methods=['GET', 'POST'])
 def add_user():
     error = None
@@ -94,24 +177,27 @@ def add_user():
 
         db = get_db()
 
-        username = request.form['username']
+        tax_id = request.form['tax_id']
 
-        user = db.execute('SELECT username, password FROM users WHERE username LIKE \'%s\'' % username)
+        _tax_id = db.execute('SELECT tax_id FROM users WHERE tax_id = %s' % tax_id)
 
-        row = user.fetchall()
+        row = _tax_id.fetchall()
         #print(row)
 
         if (row is None) | (len(row) == 0):
-            if len(request.form['ssid']) < 19:
-                error = 'invalid account ssid'
+            if len(request.form['account_sid']) < 34:
+                error = 'invalid account SID'
 
-            elif len(request.form['token']) < 18:
+            elif len(request.form['auth_token']) < 32:
                 error = 'invalid authentication token'
 
             else:
 
-                db.execute('insert into users (username, password, account_ssid, auth_token) values (?, ?, ?, ?)',
-                           [request.form['username'], request.form['password'], request.form['ssid'], request.form['token']])
+                db.execute('insert into users (tax_id, precinct, sector, password, first_name, last_name,' +
+                           ' account_sid, auth_token) values (?, ?, ?, ?, ?, ?, ?, ?)',
+                           [request.form['tax_id'], request.form['precinct'], request.form['sector'],
+                            request.form['password'], request.form['first_name'], request.form['last_name'],
+                            request.form['account_sid'], request.form['auth_token']])
 
                 db.commit()
                 flash('Successfully added user')
@@ -137,33 +223,35 @@ def login():
     error = None
     if request.method == 'POST':
         db = get_db()
-        username = request.form['username']
-        password = request.form['password']
-        user = db.execute('SELECT username, password FROM users WHERE username LIKE \'%s\'' % username)
+        tax_id = request.form['tax_id']
+        user = db.execute('SELECT tax_id, password, account_sid, auth_token, last_name FROM users WHERE tax_id = %s' % tax_id)
 
         row = user.fetchall()
         if (row is not None) & (len(row) != 0):
             row = row[0]
 
-            uname = row[0]
+
+            tax_id = row[0]
             pword = row[1]
+            sid = row[2]
+            token = row[3]
+            last = row[4]
 
-            print(uname)
-            print(pword)
-            #uname = ""
-            #pw = ""
 
-            #for usr in users:
-            #    uname = usr.username
-            #    pw = usr.password
+            print(request.form['tax_id'])
+            print(tax_id)
 
-            if request.form['username'] != uname: #user[1:2]:
-                error = 'Invalid username'
-                #error = user
+            if int(request.form['tax_id']) != int(tax_id):
+                error = 'Invalid tax_id'
+
             elif request.form['password'] != pword:
                 error = 'Invalid password'
+
             else:
                 session['logged_in'] = True
+                session['user'] = last
+                session['sid'] = sid
+                session['token'] = token
                 flash('You were logged in')
                 # return redirect(url_for('show_entries'))
 
@@ -175,6 +263,9 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    session.pop('user', None)
+    session.pop('sid', None)
+    session.pop('token', None)
     flash('You were logged out')
     return redirect(url_for('login'))
 
--- schema.sql
+++ schema.sql
@@ -6,8 +6,12 @@ create table entries (
 );
 create table users (
   id integer primary key autoincrement,
-  username text unique not null,
+  tax_id integer check(tax_id not null and length(tax_id) == 9),
+  precinct text not null,
+  sector text not null,
   password text not null,
-  account_ssid text check(account_ssid not null and length(account_ssid) == 19),
-  auth_token text check(auth_token not null and length(auth_token) == 18)
+  first_name text not null,
+  last_name text not null,
+  account_sid text check(account_sid not null and length(account_sid) == 34),
+  auth_token text check(auth_token not null and length(auth_token) == 32)
 );
